a75f5c986b625dcd86c06523cbbd37ee6fd6d5a4
Michael Zorin <michael.zorin.rd@gmail.com>
2024-01-24 19:36:17 +0100

    translate status function

--- src/components/ColumnList/ColumnList.jsx
+++ src/components/ColumnList/ColumnList.jsx
@@ -1,26 +1,43 @@
 import Column from "../Column/Column";
 
-export const statusList = [
+export const translateStatus = (russianStatus) => {
+    switch (russianStatus) {
+        case 'Без статуса':
+            return 'No status';
+        case 'Нужно сделать':
+            return 'To Do';
+        case 'В работе':
+            return 'In process';
+        case 'Тестирование':
+            return 'Testing';
+        case 'Готово':
+            return 'Ready';
+        default:
+            return russianStatus;
+    }
+};
+
+export const statusListENG = [
+    'No status',
+    'To Do',
+    'In process',
+    'Testing',
+    'Ready',
+];
+
+export const statusListRU = [
     'Без статуса',
     'Нужно сделать',
     'В работе',
     'Тестирование',
     'Готово',
 ];
-/*
-IF CASE
-export const statusList = {}
-    'No status',
-    'To Do',
-    'В работе',
-    'Testing',
-    'Ready',
-};*/
+
 
 export function ColumnList({ cardList }) {
     console.log(cardList);
     return (
-        statusList.map((status) => (
+        translateStatus(statusListRU).map((status) => (
             <Column
                 key={status}
                 title={status}
